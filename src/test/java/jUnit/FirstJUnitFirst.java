2f001ba5c568f872f1593a10352d1a7ea8e48a4c
ViraTarkovska2002 <wiara2002@gmail.com>
2024-07-22 16:08:30 +0300

    added .test life cycle

---
+++ src/test/java/jUnit/FirstJUnitFirst.java
@@ -0,0 +1,39 @@
+package jUnit;
+
+import org.junit.jupiter.api.*;
+
+import java.lang.module.Configuration;
+
+public class FirstJUnitFirst {
+    @BeforeAll
+    static void beforeAll() {
+     //   Configuration.browser = Firefox;
+        System.out.println("Это метод @BeforeAll");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Это метод @AfterAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("    Это метод @BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("    Это метод @AfterEach");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("            Это first тест");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("            Это second тест");
+    }
+}
